be70b7f61fc3825eea4fccd61af8f6263f6c3b87
Mateusz_Gawel <Mateusz_Gawel@epam.com>
2015-10-29 16:41:44 +0100

    removed public from interface method

--- src/main/java/com/epam/training/auction/service/AuthService.java
+++ src/main/java/com/epam/training/auction/service/AuthService.java
@@ -1,5 +1,5 @@
 package com.epam.training.auction.service;
 
 public interface AuthService {
-    public void registerUser(String username, String password);
+    void registerUser(String username, String password);
 }
