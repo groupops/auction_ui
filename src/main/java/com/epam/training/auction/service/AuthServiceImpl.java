aa237c3c1978f2c20d4de3fc1e193e91ef2d96b6
Adam Bronowicki <adam_bronowicki@epam.com>
2015-10-30 11:35:32 +0100

    added Camel integration

--- src/main/java/com/epam/training/auction/service/AuthServiceImpl.java
+++ src/main/java/com/epam/training/auction/service/AuthServiceImpl.java
@@ -1,11 +1,13 @@
 package com.epam.training.auction.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.epam.training.auction.common.UserTransferObject;
 import com.epam.training.auction.common.UsersService;
+import org.apache.camel.util.IOHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public final class AuthServiceImpl implements AuthService{
@@ -17,7 +19,21 @@ public final class AuthServiceImpl implements AuthService{
     
     @Override
     public void registerUser(String username, String password) {
-        usersService.addUser(new UserTransferObject(-1, username, bCryptPasswordEncoder.encode(password)));
+      AbstractApplicationContext context =
+          new ClassPathXmlApplicationContext("camel-client-remoting.xml");
+      UsersService
+          usersService =
+          context.getBean("usersServiceImplProxy", UsersService.class);
+
+      System.out.println("Invoking the logging");
+      UserTransferObject userTransferObject =
+          new UserTransferObject("user", "pass");
+      usersService.addUser(userTransferObject);
+      System.out.println("User is logged");
+
+      IOHelper.close(context);
+
+       //usersService.addUser(new UserTransferObject(-1, username, bCryptPasswordEncoder.encode(password)));
     }
 
 }
