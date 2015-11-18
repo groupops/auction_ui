bd4b97551bbcb292d4582aa182deb75e5bf6fb5b
Adam Bronowicki <adam_bronowicki@epam.com>
2015-11-18 09:44:17 +0100

    minor changes

--- src/main/java/com/epam/training/auction/controllers/AuctionController.java
+++ src/main/java/com/epam/training/auction/controllers/AuctionController.java
@@ -19,78 +19,58 @@ import static com.epam.training.auction.common.AuctionTransferObject.getBuilder;
 @Controller
 @RequestMapping(value = "/auctions")
 public final class AuctionController {
-  private final UsersService usersService;
-  private final AuctionsService auctionsService;
-  private final BiddingService biddingService;
+    private final UsersService usersService;
+    private final AuctionsService auctionsService;
+    private final BiddingService biddingService;
 
-  @Autowired
-  public AuctionController(UsersService usersService,
-                           AuctionsService auctionsService,
-                           BiddingService biddingService) {
-    this.usersService = usersService;
-    this.auctionsService = auctionsService;
-    this.biddingService = biddingService;
-  }
+    @Autowired
+    public AuctionController(UsersService usersService, AuctionsService auctionsService, BiddingService biddingService) {
+        this.usersService = usersService;
+        this.auctionsService = auctionsService;
+        this.biddingService = biddingService;
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/")
-  public ModelAndView getAuctions() {
-    ModelAndView model = new ModelAndView();
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public ModelAndView getAuctions() {
+        ModelAndView model = new ModelAndView();
 
-    List<AuctionTransferObject> activeAuctions =
-        auctionsService.getActiveAuctions();
+        List<AuctionTransferObject> activeAuctions = auctionsService.getActiveAuctions();
 
-    model.addObject("activeAuctions", activeAuctions);
-    model.setViewName("auctions");
-    return model;
-  }
+        model.addObject("activeAuctions", activeAuctions);
+        model.setViewName("auctions");
+        return model;
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/add")
-  public String showAddAuction() {
-    return "auctionAdd";
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    public String showAddAuction() {
+        return "auctionAdd";
+    }
 
-  @RequestMapping(value = "/add", method = RequestMethod.POST)
-  public String addAuction(@RequestParam String title,
-                           @RequestParam String description,
-                           @AuthenticationPrincipal User currentUser) {
-    UserTransferObject userTransferObject =
-        new UserTransferObject(currentUser.getId(), currentUser.getUsername(),
-            currentUser.getPassword());
-    AuctionTransferObject auction =
-        getBuilder(title, userTransferObject).setDescription(description)
-            .build();
-    auctionsService.addAuction(auction);
-    return "redirect:/auctions";
-  }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addAuction(@RequestParam String title, @RequestParam String description, @AuthenticationPrincipal User currentUser) {
+        UserTransferObject userTransferObject = new UserTransferObject(currentUser.getId(), currentUser.getUsername(), currentUser.getPassword());
+        AuctionTransferObject auction = getBuilder(title, userTransferObject).setDescription(description).build();
+        auctionsService.addAuction(auction);
+        return "redirect:/auctions";
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/{auctionId}")
-  public ModelAndView getAuction(@PathVariable Long auctionId,
-                                 @AuthenticationPrincipal User currentUser) {
-    ModelAndView model = new ModelAndView();
-    model.setViewName("auction");
-    UserTransferObject userTransferObject =
-        new UserTransferObject(currentUser.getId(), currentUser.getUsername(),
-            currentUser.getPassword());
-    AuctionTransferObject auction = getBuilder(auctionsService
-        .getAuctionById(auctionId).getTitle(), userTransferObject)
-        .setDescription(
-            auctionsService.getAuctionById(auctionId).getDescription())
-        .setId(auctionId).build();
-    model.addObject("auction", auction);
-    model.addObject(auctionsService.getAuctionById(auctionId));
-    return model;
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/{auctionId}")
+    public ModelAndView getAuction(@PathVariable Long auctionId, @AuthenticationPrincipal User currentUser) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("auction");
+        UserTransferObject userTransferObject = new UserTransferObject(currentUser.getId(), currentUser.getUsername(), currentUser.getPassword());
+        AuctionTransferObject auction = getBuilder(auctionsService.getAuctionById(auctionId).getTitle(), userTransferObject)
+                .setDescription(auctionsService.getAuctionById(auctionId).getDescription()).setId(auctionId).build();
+        model.addObject("auction", auction);
+        model.addObject(auctionsService.getAuctionById(auctionId));
+        return model;
+    }
 
-  @RequestMapping(method = RequestMethod.POST, value = "/bid")
-  public String doBid(@RequestParam Long auctionId, @RequestParam Long amount,
-                      @RequestParam UserTransferObject userTransferObject,
-                      @AuthenticationPrincipal User currentUser,
-                      RedirectAttributes redirectAttrs) {
-    biddingService
-        .bid(new UserBidTransferObject(userTransferObject, auctionId, amount));
-    redirectAttrs.addFlashAttribute("auctionId", auctionId)
-        .addFlashAttribute("message",
-            "There's no error actually but also there's no camel so I am faking this error message because nothing happened.");
-    return "redirect:/auctions/" + auctionId;
-  }
+    @RequestMapping(method = RequestMethod.POST, value = "/bid")
+    public String doBid(@RequestParam Long auctionId, @RequestParam Long amount, @RequestParam UserTransferObject userTransferObject, @AuthenticationPrincipal User currentUser, RedirectAttributes redirectAttrs) {
+        biddingService.bid(new UserBidTransferObject(userTransferObject, auctionId, amount));
+        redirectAttrs.addFlashAttribute("auctionId", auctionId)
+                .addFlashAttribute("message", "There's no error actually but also there's no camel so I am faking this error message because nothing happened.");
+        return "redirect:/auctions/" + auctionId;
+    }
 }
